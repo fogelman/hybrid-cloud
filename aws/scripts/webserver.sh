0dfaa48c6989fc6153b36fb680691dbdaf934172
Fogelman <ddfogelman@gmail.com>
2019-10-01 16:08:44 -0300

    UPDATE - script

--- aws/scripts/webserver.sh
+++ aws/scripts/webserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update -y
-touch index.txt
-echo 'test' > index.txt
+curl -O https://raw.githubusercontent.com/Fogelman/hybrid-cloud/master/aws/scripts/webserver.sh -o ~/etc/init.d/webserver.sh
+chmod +x /etc/init.d/webserver.sh
