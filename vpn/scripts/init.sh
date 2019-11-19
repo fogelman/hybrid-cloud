ac927990631f0c1693e290d5e562861dbbcbdd67
Fogelman <ddfogelman@gmail.com>
2019-11-19 11:07:40 -0300

    fixing client

--- vpn/scripts/init.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update -y
-sudo apt install openvpn easy-rsa -y
-make-cadir ~/openvpn-ca
-cd ~/openvpn-ca
-nano vars
--- vpn/scripts/vars.sh
+++
@@ -1,8 +0,0 @@
-export KEY_COUNTRY="US"
-export KEY_PROVINCE="NY"
-export KEY_CITY="New York City"
-export KEY_ORG="DigitalOcean"
-export KEY_EMAIL="admin@example.com"
-export KEY_OU="Community"
-
-export KEY_NAME="server"
