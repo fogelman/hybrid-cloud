a1a9a0afd7a4898e1cb2d2e1074e9896e0d62107
Fogelman <ddfogelman@gmail.com>
2019-11-21 13:27:02 -0300

    adding mongo script

--- aws/scripts/mongo.sh
+++ aws/scripts/mongo.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt update
-sudo apt install -y mongodb-org
-echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-org-shell hold" | sudo dpkg --set-selections
-echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-echo "mongodb-org-tools hold" | sudo dpkg --set-selections
-sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
+apt install mongodb
+sed -i 's/bindIp = 127.0.0.1/bindIp = 0.0.0.0/g' /etc/mongodb.conf
+service mongod restart
